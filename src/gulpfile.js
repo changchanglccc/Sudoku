a44ca7e89bdaece67ce154761b37fcb585596217
changchanglccc <lichong06091992@gmail.com>
2020-05-10 18:53:36 -0400

    chore: finish config part (mixed the original package with new version)

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 
 gulp.task('webpack', (done) => {
+    const webpack = require('webpack-stream');
+    const config = require('./webpack.config.js');
+    gulp.src('./js/**/*.js')
+        .pipe(webpack(config))
+        .pipe(gulp.dest('../www/js'));
     done();
 });
 
 gulp.task('less', (done) => {
+    const less = require('gulp-less');
+    gulp.src('./less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('../www/css'));
     done();
 });
 
