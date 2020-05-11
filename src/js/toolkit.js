8bfa548b61585643a8e4c37ec729511c66710dda
changchanglccc <lichong06091992@gmail.com>
2020-05-11 01:55:50 -0400

    chore: add Grid container on dashboard

--- src/js/toolkit.js
+++ src/js/toolkit.js
@@ -2,9 +2,11 @@ const matrixToolkit = {
     makeRow(value) {
         return new Array(9).fill(value);
     },
+
     makeMatrix(value = 0) {
         return Array.from({length: 9}, () => this.makeRow(value));
     },
+
     //Fisher - Yates
     shuffle(array) {
         const endIndex = array.length - 2;
